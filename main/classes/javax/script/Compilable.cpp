94e9fa654378d63c4c65eaad7e77267afe458e8c
Yao Yuan <yeaya@163.com>
2025-10-14 12:53:56 +0800

    first commit

---
+++ main/classes/javax/script/Compilable.cpp
@@ -0,0 +1,48 @@
+#include <javax/script/Compilable.h>
+
+#include <java/io/Reader.h>
+#include <java/lang/Class.h>
+#include <java/lang/ClassInfo.h>
+#include <java/lang/MethodInfo.h>
+#include <java/lang/String.h>
+#include <java/lang/reflect/Constructor.h>
+#include <java/lang/reflect/Method.h>
+#include <javax/script/CompiledScript.h>
+#include <jcpp.h>
+
+using $Reader = ::java::io::Reader;
+using $ClassInfo = ::java::lang::ClassInfo;
+using $MethodInfo = ::java::lang::MethodInfo;
+using $CompiledScript = ::javax::script::CompiledScript;
+
+namespace javax {
+	namespace script {
+
+$MethodInfo _Compilable_MethodInfo_[] = {
+	{"compile", "(Ljava/lang/String;)Ljavax/script/CompiledScript;", nullptr, $PUBLIC | $ABSTRACT, nullptr, "javax.script.ScriptException"},
+	{"compile", "(Ljava/io/Reader;)Ljavax/script/CompiledScript;", nullptr, $PUBLIC | $ABSTRACT, nullptr, "javax.script.ScriptException"},
+	{}
+};
+
+$ClassInfo _Compilable_ClassInfo_ = {
+	$PUBLIC | $INTERFACE | $ABSTRACT,
+	"javax.script.Compilable",
+	nullptr,
+	nullptr,
+	nullptr,
+	_Compilable_MethodInfo_
+};
+
+$Object* allocate$Compilable($Class* clazz) {
+	return $of($alloc(Compilable));
+}
+
+$Class* Compilable::load$($String* name, bool initialize) {
+	$loadClass(Compilable, name, initialize, &_Compilable_ClassInfo_, allocate$Compilable);
+	return class$;
+}
+
+$Class* Compilable::class$ = nullptr;
+
+	} // script
+} // javax
